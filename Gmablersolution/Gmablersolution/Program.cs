36342fe1fafab6065ed21672adb35fc42de20fc1
sonal <sonalkarle01@gmail.com>
2021-02-09 16:05:35 +0530

    Get track of 20 days

--- Gmablersolution/Gmablersolution/Program.cs
+++ Gmablersolution/Gmablersolution/Program.cs
@@ -7,6 +7,7 @@ namespace Gmablersolution
     class   gmabler
     {
         const int stake = 100;
+        const int count = 0;
                 
         public static void gamblermoney()
         {
@@ -20,16 +21,41 @@ namespace Gmablersolution
             Random random = new Random();
             int win = random.Next(0, 2);
             int result;
+            
             if (win == 1)
             {
                 result = (stake + 1);
-                Console.WriteLine("gambler Amount : " + result);
+                Console.WriteLine("gambler won");
+                
+                
 
             }
             else if (win == 0 )
             {
                 result = (stake - 1);
-                Console.WriteLine("gambler Amount : " + result);
+                Console.WriteLine("gambler loss");
+            }
+        }
+
+        public static void resignDay()
+
+        {
+            winOrloss();
+            for (int i = 1; i < stake; i++)
+            {
+                if (stake == stake / 2)
+                {
+                    Console.WriteLine("Gambler should resgin for day");
+                }
+            }
+        }
+        public static void Daytarck()
+        {
+            for (int day =1; day<20; day++)
+            {
+                winOrloss();
+                Console.WriteLine("Gambler won: " + day);
+
             }
         }
 
@@ -41,7 +67,8 @@ namespace Gmablersolution
             Console.WriteLine("Welcome to gambling solution");
             gmabler.gamblermoney();
             gmabler.winOrloss();
-            
+            gmabler.resignDay();
+            gmabler.Daytarck();
             
             
             
