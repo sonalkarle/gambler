337be158581a2f0ce98baaf0ba9780ed16a07392
sonal <sonalkarle01@gmail.com>
2021-02-09 15:45:58 +0530

    Welcome

---
+++ Gmablersolution/Gmablersolution/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Gmablersolution
+{
+
+
+    class   gmabler
+    {
+        const int stake = 100;
+                
+        public static void gamblermoney()
+        {
+            const int stake = 100;
+            int bet = 1;
+            
+        }
+       public static void winOrloss()
+        {
+            
+            Random random = new Random();
+            int win = random.Next(0, 2);
+            int result;
+            if (win == 1)
+            {
+                result = (stake + 1);
+                Console.WriteLine("gambler Amount : " + result);
+
+            }
+            else if (win == 0 )
+            {
+                result = (stake - 1);
+                Console.WriteLine("gambler Amount : " + result);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to gambling solution");
+            gmabler.gamblermoney();
+            gmabler.winOrloss();
+            
+            
+            
+            
+
+        }
+    }
+}
