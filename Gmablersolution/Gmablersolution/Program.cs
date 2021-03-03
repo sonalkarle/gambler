256324dd5e8a546ee6be845c4065d2581c9c92b1
sonal <sonalkarle01@gmail.com>
2021-03-03 23:26:38 +0530

    Start the game in first gambler having 100rs and per bet gambler loose or win 1 rs

--- Gmablersolution/Gmablersolution/Program.cs
+++ Gmablersolution/Gmablersolution/Program.cs
@@ -4,73 +4,24 @@ namespace Gmablersolution
 {
 
 
-    class   gmabler
+    class gmabler
     {
         const int stake = 100;
         const int count = 0;
-                
+
         public static void gamblermoney()
         {
             const int stake = 100;
             int bet = 1;
-            
+
         }
-       public static void winOrloss()
-        {
-            
-            Random random = new Random();
-            int win = random.Next(0, 2);
-            int result;
-            
-            if (win == 1)
-            {
-                result = (stake + 1);
-                Console.WriteLine("gambler won");
-                
-                
-
-            }
-            else if (win == 0 )
-            {
-                result = (stake - 1);
-                Console.WriteLine("gambler loss");
-            }
-        }
-
-        public static void resignDay()
-
-        {
-            winOrloss();
-            resignDay();
-            for (int i = 1; i < stake; i++)
-            {
-                if (stake == stake / 2)
-                {
-                    Console.WriteLine("Gambler should resgin for day");
-                }
-            }
-        }
-        public static void Daytarck()
-        {
-            for (int day =1; day<20; day++)
-            {
-                winOrloss();
-                Console.WriteLine("Gambler won" );
-                Console.WriteLine("Gambler loss");
-
-            }
-        }
-
-    }
-    class Program
+        class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to gambling solution");
             gmabler.gamblermoney();
-            gmabler.winOrloss();
-            gmabler.resignDay();
-            gmabler.Daytarck();
+           
             
             
             
